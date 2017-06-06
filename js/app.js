354ef54e852a6ddd43f1ce09c81e0b15bba111bf
MTupaczewski <mactup6@gmail.com>
2017-06-06 13:53:20 +0200

    Second commit for block 9

--- js/app.js
+++ js/app.js
@@ -1,17 +1,26 @@
 /*global $*/
 var main = function () {
     'use strict';
-    $(document).keypress(function (event) {
-        if (event.which === 13) {
-            document.getElementById("logo").src = "images/about1.png";
-        } else if (event.which === 90) {
-            document.getElementById("logo1").src = "images/redmat.png";
-        } else if (event.which === 78) {
-            document.getElementById("logo2").src = "images/bluemat.png";
-        } else if (event.which === 70) {
-            document.getElementById("logo3").src = "images/dev.png";
-        }
-    });
+    var i = 1, x;
+    for (x = 0; x <= 100; x += 1) {
+        $(document).keypress(function (event) {
+            if (event.which === 13) {
+                if (i === 1) {
+                    document.getElementById("logo").src = "images/about1.png";
+                    i = i + 1;
+                } else if (i === 2) {
+                    document.getElementById("logo").src = "images/bluemat.png";
+                    i = i + 1;
+                } else if (i === 3) {
+                    document.getElementById("logo").src = "images/redmat.png";
+                    i = i + 1;
+                } else if (i === 4) {
+                    document.getElementById("logo").src = "images/dev.png";
+                    i = 1;
+                }
+            }
+        });
+    }
     $('.icon-menu').click(function () {
         $('.menu').animate({
             left: "0px"
