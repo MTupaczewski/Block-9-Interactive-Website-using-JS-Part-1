835a6cd7381bd1ed96ee111e6c7e04c945477803
MTupaczewski <mactup6@gmail.com>
2017-08-21 12:45:01 +0200

    Latest commit

--- js/app.js
+++ js/app.js
@@ -1,45 +1,35 @@
 /*global $*/
 var main = function () {
     'use strict';
-    var i = 1, x;
-    for (x = 0; x <= 100; x += 1) {
-        $(document).keypress(function (event) {
-            if (event.which === 13) {
-                if (i === 1) {
-                    document.getElementById("logo").src = "images/about1.png";
-                    i = i + 1;
-                } else if (i === 2) {
-                    document.getElementById("logo").src = "images/bluemat.png";
-                    i = i + 1;
-                } else if (i === 3) {
-                    document.getElementById("logo").src = "images/redmat.png";
-                    i = i + 1;
-                } else if (i === 4) {
-                    document.getElementById("logo").src = "images/dev.png";
-                    i = 1;
-                }
+    var i = 1;
+    $(document).keypress(function (event) {
+        if (event.which === 13) {
+            if (i === 1) {
+                document.getElementById("logo2").src = "images/about1.png";
+                i = i + 1;
+            } else if (i === 2) {
+                document.getElementById("logo2").src = "images/bluemat.png";
+                i = i + 1;
+            } else if (i === 3) {
+                document.getElementById("logo2").src = "images/redmat.png";
+                i = i + 1;
+            } else if (i === 4) {
+                document.getElementById("logo2").src = "images/dev.png";
+                i = 1;
             }
-        });
-    }
+        }
+    });
     $('.icon-menu').click(function () {
         $('.menu').animate({
             left: "0px"
         }, 200);
-
-        $('body').animate({
-            left: "285px"
-        }, 200);
     });
 
-    /* Then push them back */
     $('.icon-close').click(function () {
         $('.menu').animate({
             left: "-285px"
         }, 200);
         
-        $('body').animate({
-            left: "0px"
-        }, 200);
     });
 };
 $(document).ready(main);
